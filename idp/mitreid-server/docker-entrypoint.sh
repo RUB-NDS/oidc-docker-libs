355cbe7869a1b0c1037d2ab27c44d50becbe21bf
Christian Fries <christian.fries123@gmail.com>
2020-02-01 16:37:48 +0100

    use http proxy also for server

--- idp/mitreid-server/docker-entrypoint.sh
+++ idp/mitreid-server/docker-entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if [ ! -e /usr/local/share/ca-certificates/CA_CERT ]; then
-  cp -f CA_DIR/CA_CERT /usr/local/share/ca-certificates/CA_CERT
+if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
+  cp -f ${CA_DIR}/${CA_CERT} /usr/local/share/ca-certificates/${CA_CERT}
   update-ca-certificates
 
   keytool -keystore /usr/local/openjdk-8/lib/security/cacerts -storepass changeit -noprompt -trustcacerts \
-          -importcert -alias oidc -file /usr/share/ca-certificates/CA_CERT
+          -importcert -alias oidc -file /usr/share/ca-certificates/${CA_CERT}
 fi
 
 service apache2 start
 
-su jboss -s /bin/bash -c "/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0"
+if [ ! -z "$HTTP_PROXY_URL" ]; then
+  PROXY_SETTINGS="-Dhttp.proxyHost=${HTTP_PROXY_URL} -Dhttp.proxyPort=${HTTP_PROXY_PORT} -Dhttps.proxyHost=${HTTP_PROXY_URL} -Dhttps.proxyPort=${HTTP_PROXY_PORT} -Dhttp.nonProxyHosts='localhost'"
+fi
+
+su jboss -s /bin/bash -c "/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 $PROXY_SETTINGS"
