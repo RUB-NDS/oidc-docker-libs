3551885f42901c784801346f0f82f5e1778c512b
Christian Fries <christian.fries123@gmail.com>
2020-02-01 15:37:30 +0100

    fix mitreid-client configuration use proxy only if wanted pinned chromium version to ensure it is working with driver

--- professos/docker-entrypoint.sh
+++ professos/docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#TODO use ENV variables
-if [ ! -e /usr/local/share/ca-certificates/CA_CERT ]; then
-  cp -f CA_DIR/CA_CERT /usr/local/share/ca-certificates/CA_CERT
+if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
+  cp -f ${CA_DIR}/${CA_CERT} /usr/local/share/ca-certificates/${CA_CERT}
   update-ca-certificates
 
   keytool -keystore /usr/local/openjdk-11/lib/security/cacerts -storepass changeit -noprompt -trustcacerts \
-          -importcert -alias oidc -file /usr/share/ca-certificates/CA_CERT
+          -importcert -alias oidc -file /usr/share/ca-certificates/${CA_CERT}
 fi
 
 service apache2 start
 
-#TODO use ENV variables
-PROXY_SETTINGS="-Dhttp.proxyHost=mitmproxy -Dhttp.proxyPort=8080 -Dhttps.proxyHost=mitmproxy -Dhttps.proxyPort=8080"
+if [ ! -z "$HTTP_PROXY_URL" ]; then
+  PROXY_SETTINGS="-Dhttp.proxyHost=${HTTP_PROXY_URL} -Dhttp.proxyPort=${HTTP_PROXY_PORT} -Dhttps.proxyHost=${HTTP_PROXY_URL} -Dhttps.proxyPort=${HTTP_PROXY_PORT} -Dhttp.nonProxyHosts='localhost'"
+fi
 
 su jboss -s /bin/bash -c "/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug 0.0.0.0:8787 $PROXY_SETTINGS"
