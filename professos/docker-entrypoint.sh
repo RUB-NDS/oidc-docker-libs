e1fc5e25ffba48d984cd7ffac8f838ade5ebbf68
Christian Fries <christian.fries123@gmail.com>
2020-02-01 13:04:29 +0100

    first running version

--- professos/docker-entrypoint.sh
+++ professos/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#TODO use ENV variables
 if [ ! -e /usr/local/share/ca-certificates/CA_CERT ]; then
   cp -f CA_DIR/CA_CERT /usr/local/share/ca-certificates/CA_CERT
   update-ca-certificates
@@ -10,7 +11,7 @@ fi
 
 service apache2 start
 
+#TODO use ENV variables
 PROXY_SETTINGS="-Dhttp.proxyHost=mitmproxy -Dhttp.proxyPort=8080 -Dhttps.proxyHost=mitmproxy -Dhttps.proxyPort=8080"
-#PROXY_SETTINGS="-Djava.net.useSystemProxies=true"
 
 su jboss -s /bin/bash -c "/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug 0.0.0.0:8787 $PROXY_SETTINGS"
