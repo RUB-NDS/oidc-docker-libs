d78c43e3c3c0a19639a6d076b65ca96317f46e3b
Christian Fries <christian.fries123@gmail.com>
2020-04-10 09:37:47 +0200

    mitmproxy: mount script directory to container

--- mitmproxy/docker-entrypoint.sh
+++ mitmproxy/docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
 
-MITMPROXY_PATH="/home/mitmproxy/.mitmproxy"
-
-mkdir -p "$MITMPROXY_PATH"
-chown -R mitmproxy:mitmproxy "$MITMPROXY_PATH"
-set -x
+mkdir -p $MITMPROXY_PATH
+chown mitmproxy:mitmproxy "$MITMPROXY_PATH"
 cat ${CA_DIR}/${CA_KEY} ${CA_DIR}/${CA_CERT} > ${MITMPROXY_PATH}/mitmproxy-ca.pem
 
-RUN_CMD="mitmweb --web-iface 0.0.0.0 --set web_open_browser=false --ssl-insecure"
+# for script examples https://github.com/mitmproxy/mitmproxy/tree/master/examples
+
+set -x
+RUN_CMD="mitmweb --web-iface 0.0.0.0 \
+        --set web_open_browser=false --set stream_websockets=true \
+        --ssl-insecure \
+        -s ${HOME}/scripts/noop.py"
 
 # The following part was modified to generate Procfile with the commands
 # needed to run by forego
