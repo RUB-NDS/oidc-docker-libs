9b89dbf46caeeea5b1438683e45ce88c9418e1e6
Christian Fries <christian.fries123@gmail.com>
2020-04-18 09:38:04 +0200

    apache2 config and serve static professos file

---
+++ idp/identity-server4/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
+  cp -f ${CA_DIR}/${CA_CERT} /usr/local/share/ca-certificates/${CA_CERT}
+  update-ca-certificates
+fi
+
+service apache2 start &
+
+
+dotnet IdentityServer.dll
