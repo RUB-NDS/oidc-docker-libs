c5d1262c13693b96f8c1cf02a1ceb2ef41d9816a
Christian Fries <christian.fries123@gmail.com>
2020-05-19 14:29:10 +0200

    php-oidc-op: add phpOidc OP service

---
+++ idp/php-oidc-op/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
+  cp -f ${CA_DIR}/${CA_CERT} /usr/local/share/ca-certificates/${CA_CERT}
+  update-ca-certificates
+fi
+
+php-fpm -D
+nginx -g 'daemon off;'
+:
