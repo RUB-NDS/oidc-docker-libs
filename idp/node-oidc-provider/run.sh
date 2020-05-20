7fc0e306d8aca4857faf173fe8b8b05fe553322d
Christian Fries <christian.fries123@gmail.com>
2020-05-20 15:27:16 +0200

    node-oidc-provider: standalone example

---
+++ idp/node-oidc-provider/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+trap 'kill ${!}; term_handler' SIGHUP SIGINT SIGQUIT SIGTERM
+
+pid=0
+
+term_handler() {
+  echo "term_handler"
+  service ntp stop
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
+  cp -f ${CA_DIR}/${CA_CERT} /usr/local/share/ca-certificates/${CA_CERT}
+  update-ca-certificates
+fi
+
+service ntp start
+service apache2 start
+
+echo -n "Starting node example standalone ... "
+node example/standalone &
+#node certification/oidc &
+pid="$!"
+if [ $? -eq 0 ] ; then
+  echo "OK"
+else
+  echo "ERROR"
+  exit -1
+fi
+
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
+
+echo "exited $0"
