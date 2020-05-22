55f75902b7a74a498a6395e824684e0e4725b6d9
Christian Fries <christian.fries123@gmail.com>
2020-05-22 10:07:40 +0200

    add pyoidc rp lab entry

--- oidc-lab/app/src/app/rp-list/rp-list.component.ts
+++ oidc-lab/app/src/app/rp-list/rp-list.component.ts
@@ -10,6 +10,7 @@ export const RPLIST: RpList[] = [
   { id: 1, name: 'Angular auth oidc', url: 'https://angular-auth-oidc-client' },
   { id: 2, name: 'MITREid Client', url: 'https://mitreid-client/simple-web-app/'},
   { id: 3, name: 'PHP OIDC RP', url: 'https://php-oidc-rp' },
+  { id: 4, name: 'PyOIDC RP', url: 'https://pyoidc-rp' },
 ];
 
 
