6f376639ba347b0cf1ebdf31478b79f6f50267ad
Christian Fries <christian.fries123@gmail.com>
2020-05-23 13:26:18 +0200

    update gravitee nginx

---
+++ idp/gravitee/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+while [[ ! -e ${CA_DIR}/${VIRTUAL_HOST}/${VIRTUAL_HOST}.crt ]] ; do
+    sleep 1
+done
+
+if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
+  cp -f ${CA_DIR}/${CA_CERT} /usr/local/share/ca-certificates/${CA_CERT}
+  update-ca-certificates
+fi
+
+nginx -g 'daemon off;'
