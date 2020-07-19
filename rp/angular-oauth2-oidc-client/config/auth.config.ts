e70613d14fcbbf8a318c8a23e128355754f07aef
Christian Fries <christian.fries123@gmail.com>
2020-07-19 13:58:00 +0200

    save angular test client config for professos

--- rp/angular-oauth2-oidc-client/config/auth.config.ts
+++ rp/angular-oauth2-oidc-client/config/auth.config.ts
@@ -1,10 +1,10 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'https://keycloak/auth/realms/master',
+    issuer: 'https://attack-idp.professos/angular-oauth2-oidc',
     redirectUri: window.location.origin + '/index.html',
     clientId: 'angular-oauth2-oidc-client',
-    responseType: 'id_token token',
+    responseType: 'code',
     scope: 'openid profile email',
     showDebugInformation: true,
     timeoutFactor: 0.01
