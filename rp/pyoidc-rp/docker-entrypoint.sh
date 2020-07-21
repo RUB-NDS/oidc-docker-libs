8dfb0bf9226e41d61c22133b3d9b8cb8332ae1c8
Christian Fries <christian.fries123@gmail.com>
2020-07-21 18:53:36 +0200

    wait for keycloak and professos

--- rp/pyoidc-rp/docker-entrypoint.sh
+++ rp/pyoidc-rp/docker-entrypoint.sh
@@ -9,6 +9,25 @@ if [ ! -e /usr/local/share/ca-certificates/${CA_CERT} ]; then
   update-ca-certificates
 fi
 
+while true; do
+  response=$(curl -o /dev/null -s -w "%{http_code}\n" https://honest-idp.professos/pyoidc-rp/.well-known/openid-configuration)
+  if [[ "$response" != "200" ]]; then
+    curl -X POST -d test_id=pyoidc-rp -o /dev/null https://professos/api/rp/create-test-object
+    curl -X POST -H "Content-type: application/json" -o /dev/null 'https://professos/api/rp/pyoidc-rp/expose/ID%20Spoofing%201%20-%20ID%20Token%20(sub)'
+  elif [[ "$response" == "200" ]]; then
+    break;
+  fi
+  sleep 2
+done
+
+while true; do
+  response=$(curl -o /dev/null -s -w "%{http_code}\n" https://keycloak/auth/realms/master/.well-known/openid-configuration)
+  if [[ "$response" == "200" ]]; then
+    break;
+  fi
+  sleep 2
+done
+
 export BASE_URI=https://${VIRTUAL_HOST}
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export FLASK_APP=oidc
