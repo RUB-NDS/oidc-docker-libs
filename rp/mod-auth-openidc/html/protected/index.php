d6527fd1e0cef4404ebba4febe1e2107209729aa
Christian Fries <christian.fries123@gmail.com>
2020-08-01 10:57:44 +0200

    mod-auth-openidc: print better user needle claim

--- rp/mod-auth-openidc/html/protected/index.php
+++ rp/mod-auth-openidc/html/protected/index.php
@@ -1,7 +1,13 @@
 <html>
   <body>
     <h1>Hello, <?php echo($_SERVER['REMOTE_USER']) ?></h1>
-    <pre><?php print_r(array_map("htmlentities", apache_request_headers())); ?></pre>
+    <pre><?php
+        $claims = array_map("htmlentities", apache_request_headers());
+        echo("<h2> Full claims </h2>");
+        print_r($claims);
+        echo("<h2> Full claims </h2>");
+        echo("{sub=" . $claims["OIDC_CLAIM_sub"] . ", iss=" . $claims["OIDC_CLAIM_iss"] . "}");
+    ?></pre>
     <a href=<?php echo "/protected/redirect_uri?logout=https%3A%2F%2F" .  $_SERVER['SERVER_NAME'] . "%2Flogout.html"; ?> >Logout</a>
   </body>
 </html>
