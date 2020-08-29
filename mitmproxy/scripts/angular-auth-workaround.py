0fc619341bd2a28f7248c9d9f198b5631981d220
Christian Fries <christian.fries123@gmail.com>
2020-08-29 09:16:51 +0200

    answer CORS OPTION check also for angular auth2

--- mitmproxy/scripts/angular-auth-workaround.py
+++ mitmproxy/scripts/angular-auth-workaround.py
@@ -14,3 +14,15 @@ def request(flow: http.HTTPFlow) -> None:
              "X-XSS-Protection": "1; mode=block",
              "Strict-Transport-Security": "max-age=31536000; includeSubDomains"}  # (optional) headers
         )
+    if flow.request.pretty_url == "https://attack-idp.professos/angular-oauth2-oidc/user-info" and flow.request.method == "OPTIONS":
+        ctx.log.info("Intercept angular auth oidc OPTIONS request")
+        flow.response = http.HTTPResponse.make(
+            200,
+            b"",  # (optional) content
+            {"Access-Control-Allow-Origin": "https://angular-oauth2-oidc-client",
+             "Access-Control-Allow-Credentials": "true",
+             "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS",
+             "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+             "X-XSS-Protection": "1; mode=block",
+             "Strict-Transport-Security": "max-age=31536000; includeSubDomains"}  # (optional) headers
+        )
