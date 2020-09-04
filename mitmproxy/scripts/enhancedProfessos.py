1b2aff520c2e851964a18adef3986f932f06e3ff
Christian Fries <christian.fries123@gmail.com>
2020-09-04 17:51:52 +0200

    update mitm script

--- mitmproxy/scripts/enhancedProfessos.py
+++ mitmproxy/scripts/enhancedProfessos.py
@@ -1,13 +1,32 @@
 from mitmproxy import ctx, http
+import base64
+import json
 import threading
 import socketserver
 
 
+class Intercept:
+    def __init__(self, search, replace):
+        self.search = base64.b64decode(search).decode('ascii')
+        self.replace = base64.b64decode(replace).decode('ascii')
+
+
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+
     def handle(self):
         data = str(self.request.recv(1024), 'ascii')
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        cmd = json.loads(data)
+
+        if cmd.get("type") == 'clear':
+            self.server.controller.clear()
+        elif cmd.get("type") == 'request':
+            intercept = Intercept(cmd.get('search'), cmd.get('replace'))
+            self.server.controller.requestInterceptor = intercept
+        elif cmd.get("type") == 'response':
+            intercept = Intercept(cmd.get('search'), cmd.get('replace'))
+            self.server.controller.responseInterceptor = intercept
+
+        response = bytes("OK", 'ascii')
         self.request.sendall(response)
 
 
@@ -21,7 +40,28 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 class Controller(object):
     def __init__(self):
-        self.text = "Hello"
+        self.__requestInterceptor = []
+        self.__responseInterceptor = []
+
+    def clear(self):
+        self.__requestInterceptor.clear()
+        self.__responseInterceptor.clear()
+
+    @property
+    def requestInterceptor(self):
+        return self.__requestInterceptor
+
+    @requestInterceptor.setter
+    def requestInterceptor(self, value):
+        self.__requestInterceptor.append(value)
+
+    @property
+    def responseInterceptor(self):
+        return self.__responseInterceptor
+
+    @responseInterceptor.setter
+    def responseInterceptor(self, value):
+        self.__responseInterceptor.append(value)
 
 
 class ProfessosEnhancer(object):
@@ -44,12 +84,20 @@ class ProfessosEnhancer(object):
         server_thread.start()
         ctx.log.info("Enhancer listens on {}:{}".format(ip,port))
 
-
     def request(self, flow: http.HTTPFlow) -> None:
-        ctx.log.info("Request: {}".format(self.controller.text))
+        #ctx.log.info("Request {}".format(flow.request.pretty_url))
+        for intercept in self.controller.requestInterceptor:
+            ctx.log.info("Request {}".format(intercept.search))
+            if flow.request.pretty_url == intercept.search:
+                #flow.request.url
+                pass
 
     def response(self, flow: http.HTTPFlow) -> None:
-        pass
+        for intercept in self.controller.responseInterceptor:
+            ctx.log.info("{}".format(intercept.search))
+            if flow.request.pretty_url == intercept.search:
+                #flow.response = http.HTTPResponse.make(status, content, header)
+                pass
 
     def done(self):
         if self.server is not None:
