8ab84c2ab971d05aa9627d8bf47ad0ba92f2bed4
Louis Jannett <louis.jannett@rub.de>
2021-05-14 03:28:28 -0700

    Wait for generated certs before container start

--- rp/express-openid-client/docker-entrypoint.sh
+++ rp/express-openid-client/docker-entrypoint.sh
@@ -14,6 +14,10 @@ term_handler() {
   exit 143; # 128 + 15 -- SIGTERM
 }
 
+if [ ! -z CA_DIR ] && [ ! -z CA_CERT ]; then
+  while [ ! -f "${CA_DIR}/${CA_CERT}" ]; do echo "ca cert not generated yet - sleeping"; sleep 1; done;
+fi;
+
 while [[ ! -e ${CA_DIR}/${VIRTUAL_HOST}/${VIRTUAL_HOST}.crt ]] ; do
     sleep 1
 done
